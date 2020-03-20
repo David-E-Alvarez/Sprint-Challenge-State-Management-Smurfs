c4b7ae069d92339273c2962ffa71af0d083b46b6
david <davidealvarez1@gmail.com>
2020-03-20 11:01:30 -0700

    non broken code but not mvp

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -3,7 +3,8 @@ import axios from "axios";
 import "./App.css";
 import {smurfContext} from '../contexts/smurfContext';
 import Smurfs from "./Smurfs";
-const App = () =>{
+
+function App(){
     const [smurfData, setSmurfData] = useState([]);
     useEffect(() => {
       axios
@@ -13,12 +14,30 @@ const App = () =>{
         setSmurfData(res.data);
       })
     },[])
+    const addNewSmurf = smurf => {
+      const newSmurf = {
+        name: smurf.name,
+        age: smurf.age,
+        height: smurf.height
+      };
+    }
+    const addName = name => {
+      console.log("name", name.target.value)
+      setSmurfData([...smurfData, name.target.value])
+      // add the given item to the cart
+    };
+
+      // const submitForm = e => {
+      //   e.preventDefault();
+      //   addNewSmurf(teamMember);
+      //   setSmurfData({ name: "", age: "",height: "" });
+      // };
     return (
       <div className="App">
         <h1>The Smurfs</h1>
-        <form>
+            <form>
                 <label htmlFor="name">Smurf name: </label>
-                <input id="name" type="text" placeholder="add smurf" name="name"/><br/>
+                <input id="name" type="text" placeholder="add smurf" name="name" onChange={addName}/><br/>
                 <label htmlFor="age">Age: </label>
                 <input id="age" type="text" placeholder="add age" name="email"/><br/>
                 <label htmlFor="height">Height: </label>
@@ -30,7 +49,6 @@ const App = () =>{
         </smurfContext.Provider>
       </div>
     );
-  }
-
+}
 
 export default App;
