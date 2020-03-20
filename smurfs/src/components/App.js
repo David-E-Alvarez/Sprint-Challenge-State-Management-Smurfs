8bee70b319c5dfd4c958df4ca669f2bc3e4ff889
david <davidealvarez1@gmail.com>
2020-03-20 14:16:28 -0700

    mvp

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,4 +1,4 @@
-import React, { Component, useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "./App.css";
 import {smurfContext} from '../contexts/smurfContext';
@@ -6,42 +6,57 @@ import Smurfs from "./Smurfs";
 
 function App(){
     const [smurfData, setSmurfData] = useState([]);
-    useEffect(() => {
+    const [smurf, setSmurf] = useState({
+      name: "", 
+      age: null,
+      height: null
+    })
+
+    const getSmurfs = () => {
       axios
       .get("http://localhost:3333/smurfs")
       .then(res => {
         console.log("res.data", res.data)
         setSmurfData(res.data);
       })
-    },[])
-    const addNewSmurf = smurf => {
-      const newSmurf = {
-        name: smurf.name,
-        age: smurf.age,
-        height: smurf.height
-      };
     }
-    const addName = name => {
-      console.log("name", name.target.value)
-      setSmurfData([...smurfData, name.target.value])
-      // add the given item to the cart
-    };
+    useEffect(getSmurfs,[])
+      const submitForm = e => {
+        e.preventDefault();
+        axios
+          .post("http://localhost:3333/smurfs", smurf)
+          .then(res => {
+            console.log("res.data", res.data)
+            getSmurfs()
+          })
+          .catch(err =>{
+            console.log("err", err)
+          })
+          setSmurf({
+            name: "", 
+            age: null,
+            height: null
+          })
+      };
 
-      // const submitForm = e => {
-      //   e.preventDefault();
-      //   addNewSmurf(teamMember);
-      //   setSmurfData({ name: "", age: "",height: "" });
-      // };
+      const handleChange = e => {
+        setSmurf({ 
+            ...smurf, [e.target.name]: e.target.value
+        });
+       
+        console.log("e.target.value in handleNameChange: ", e.target.value);
+      };
+
     return (
       <div className="App">
         <h1>The Smurfs</h1>
-            <form>
+            <form onSubmit={submitForm}>
                 <label htmlFor="name">Smurf name: </label>
-                <input id="name" type="text" placeholder="add smurf" name="name" onChange={addName}/><br/>
+                <input id="name" type="text" placeholder="add smurf" name="name" onChange={handleChange}/><br/>
                 <label htmlFor="age">Age: </label>
-                <input id="age" type="text" placeholder="add age" name="email"/><br/>
+                <input id="age" type="text" placeholder="add age" name="age" onChange={handleChange}/><br/>
                 <label htmlFor="height">Height: </label>
-                <input id="height" type="text" placeholder="add height" name="role"/><br/>
+                <input id="height" type="text" placeholder="add height" name="height" onChange={handleChange}/><br/>
                 <button type="submit">Add Smurf</button>
             </form>
         <smurfContext.Provider value={{smurfData}}>
